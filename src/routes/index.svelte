d7aa3c8bce9ffee6e2f7d51da551f8916565c052
Tim Hårek Andreassen <tim@harek.dev>
2022-05-03 20:16:45 +0200

    feat: Add reverse coordinates lookup
    Signed-off-by: Tim Hårek Andreassen <tim@harek.dev>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,15 @@
   import Table from '../components/Table.svelte';
   import Now from '../components/Now.svelte';
   import type { YrWeather } from 'src/types/YrWeather';
+  import type { NominatimReverse } from 'src/types/Nominatim';
 
-  let LOCATION = { lat: 59.355091, lng: 5.323378 }; // Vormedal coordinates
-  let weatherData: YrWeather;
+  let LOCATION = { name: 'Vormedal', lat: 59.355091, lng: 5.323378 }; // Vormedal coordinates
 
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((location) => {
+      navigator.geolocation.getCurrentPosition(async (location) => {
         LOCATION = {
+          name: await getNameFromCoordinates(location.coords.latitude, location.coords.longitude),
           lat: location.coords.latitude,
           lng: location.coords.longitude
         };
@@ -22,6 +23,12 @@
 
   async function getYr(lat: string, lng: string): Promise<YrWeather> {
     const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lng}`;
+    const result: Promise<YrWeather> = await _fetch(url);
+
+    return result;
+  }
+
+  async function _fetch(url: string): Promise<any> {
     const result = await fetch(url, {
       method: 'GET',
       headers: {
@@ -33,7 +40,25 @@
         console.log(error);
       });
 
-    weatherData = result;
+    return result;
+  }
+
+  async function getNameFromCoordinates(lat: number, lon: number) {
+    const result = await _getNameFromCoordinates(lat, lon);
+
+    if (result.address.city) {
+      return result.address.city;
+    } else if (result.address.village) {
+      return result.address.village;
+    }
+
+    return 'Ukjent';
+  }
+
+  async function _getNameFromCoordinates(lat: number, lon: number): Promise<NominatimReverse> {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`;
+    const result: Promise<NominatimReverse> = await _fetch(url);
+
     return result;
   }
 </script>
@@ -43,6 +68,10 @@
   {#if LOCATION}
     <div class="my-4">
       <dl>
+        <div class="flex gap-1">
+          <dt>Sted:</dt>
+          <dd>{LOCATION.name}</dd>
+        </div>
         <div class="flex gap-1">
           <dt>Breddegrad:</dt>
           <dd>{LOCATION.lat}</dd>
@@ -56,8 +85,8 @@
     {#await getYr(String(LOCATION.lat), String(LOCATION.lng))}
       <div class="">laster inner...</div>
     {:then result}
-      <Now now={weatherData.properties.timeseries[0]} />
-      <Table {weatherData} />
+      <Now now={result.properties.timeseries[0]} />
+      <Table weatherData={result} />
     {/await}
   {/if}
 </main>
