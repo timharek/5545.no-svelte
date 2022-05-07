29b93ffa3fbab2bdb4f809e7e5ea29e76c759116
Tim Hårek Andreassen <tim@harek.dev>
2022-05-07 23:24:51 +0200

    fix: Add title and Plausible to svelte:head
    Signed-off-by: Tim Hårek Andreassen <tim@harek.dev>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,10 +6,10 @@
   import '../css/main.css';
 </script>
 
-<head>
+<svelte:head>
   <title>5545.no</title>
   <script defer data-domain="5545.no" src="https://plausible.io/js/plausible.js"></script>
-</head>
+</svelte:head>
 <Header title={locationName} message="🦺 Under utvikling 🦺" />
 
 <slot />
