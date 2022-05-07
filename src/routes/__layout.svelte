b99f85e0bc8a71607b23ce6e705fcd2fde9af0a7
Tim Hårek Andreassen <tim@harek.dev>
2022-05-07 23:21:13 +0200

    feat: Add Plausible Analytics
    Signed-off-by: Tim Hårek Andreassen <tim@harek.dev>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,6 +8,7 @@
 
 <head>
   <title>5545.no</title>
+  <script defer data-domain="5545.no" src="https://plausible.io/js/plausible.js"></script>
 </head>
 <Header title={locationName} message="🦺 Under utvikling 🦺" />
 
